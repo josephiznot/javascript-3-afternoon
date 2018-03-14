3ef18ca4d0b8356a6393e47d09d922e98904688d
Joseph Dartlan Anderson <J.D.Andy@Andys-MacBook-Pro.local>
2018-03-14 00:40:15 -0500

    initial commit

--- classes.js
+++ classes.js
@@ -29,9 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    (this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.email = email),
+      (this.age = age);
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,11 +56,22 @@
 
   Call your new class Manager
 */
-
-//Code Here
-
-
-
+class Manager {
+  constructor(first_name, last_name, email, age, reports) {
+    (this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.email = email),
+      (this.age = age),
+      (this.reports = []);
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+  }
+}
+// Object.assign(Manager, Employee);
 ////////// PROBLEM 3 //////////
 
 /*
@@ -74,10 +93,38 @@
 
   Call your new class ProgressiveManager
 */
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age, reports, title, bonus) {
+    (this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.email = email),
+      (this.age = age),
+      (this.reports = []),
+      (this.title = "Not a manager"),
+      (this.bonus = 0);
+  }
+  hire(employee) {
+    this.reports.push(employee);
+    if (this.reports.length >= 101) {
+      this.title = "Bestest Manager";
+    } else if (this.reports.length > 50) {
+      this.title = "Manager Plus";
+    } else if (this.reports.length > 10) {
+      this.title = "Manager";
+    } else if (this.reports.length > 3) {
+      this.title = "Mostly Manager";
+    } else if (this.reports.length > 0) {
+      this.title = "Barely Manager";
+    } else {
+      this.title = "Barely Manager";
+    }
+  }
+  fire(index) {
+    this.reports.splice(index, 1), (this.bonus += 100);
+  }
+}
 
 //Code Here
-
-
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -103,5 +150,26 @@
 */
 
 //Code Here
-
-
+class Machine {
+  constructor() {
+    (this.widgets_made_count = 0),
+      (this.wear_and_tear_count = 0),
+      (this.needs_reboot = false);
+  }
+  makeWidgets(number) {
+    this.widgets_made_count += number;
+    if (this.widgets_made_count % 50 === 0) {
+      this.wear_and_tear_count += this.widgets_made_count / 50;
+    }
+    return this.wear_and_tear_count;
+  }
+  fixMachine() {
+    return (this.need_reboot = true);
+  }
+  reboot(cb) {
+    if (this.needs_reboot === true) {
+      wear_and_tear_count -= 10;
+      needs_reboot = false;
+    }
+  }
+}
